c50da90f589ff6e61a5227115ded0658de88d86a
Jiya Chowdhury <138868637+jc-2402@users.noreply.github.com>
2024-06-26 23:16:45 +0530

    Add files via upload

---
+++ basicsorting4.java
@@ -0,0 +1,33 @@
+//counting sort
+public class basicsorting4{
+    public static void countingsort(int arr[]){
+        //LARGEST IS used to find range like 1 to 7
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++; //filling count array
+        }
+        //sorting by tranfering from count to original array
+        int j=0;
+        for(int i=0; i<count.length; i++){
+            while(count[i]>0){
+                arr[j] =i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,4,1,3,2,4,3,7};
+        countingsort(arr);
+        printarr(arr);
+    }
+}
---
+++ basicsorting6.java
@@ -0,0 +1,27 @@
+//selection sort in descending order
+public class basicsorting6{
+    public static void selection(int arr[]){
+        for(int i=0; i<arr.length-1 ;i++){
+            int minpos = i;
+            for(int j=i+1; j<arr.length; j++){
+                if( arr[minpos] < arr[j]){
+                    minpos = j;
+                }
+            }
+            //swap
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={5,4,1,3,2};
+        selection(arr);
+        printarr(arr);
+    }
+}
